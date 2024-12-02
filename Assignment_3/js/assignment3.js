dda553a3a02baa405462f03ae0aa82e2dd6a66c7
Joshva-D <joshvadani38@gmail.com>
2024-12-02 18:13:54 +0530

    Fixed bugs

--- Assignment_3/js/assignment3.js
+++ Assignment_3/js/assignment3.js
@@ -1,4 +1,3 @@
-
 // 1. {a:'one', b:'2', f:'5', c:'33', p:'do', q:'one' }: Using this object make an array consisting of all the keys of the given object in reverse order. (output should be ['q', 'p', 'c', 'f', 'b', 'a'])
 
 let obj1 = {
@@ -29,48 +28,43 @@ const arr3 = arr2.filter((obj) => obj.id !== "24");
 
 console.log(arr3);
 
-// 3. Write a function to 
+// 3. Write a function to
 // a. Calculate the height of the end user's browser screen
 // b. To console the name of the web host
 // c. To show a warning message if there is no https protocol used in the visited website.
 // d. To show an alert message after 10sec while the page is refereshed.
 
-function question3() {
+function objectModels() {
   console.log(screen.height);
   console.log(location.host);
   if (!location.protocol.includes("https")) {
     console.warn("Https protocol not used");
   }
   setTimeout(function () {
-    alert("Its an alert message after 10 sec webpage loads");
+    alert("Its an alert message after 10 sec webpage loaded");
   }, 10000);
 }
 
-question3();
+objectModels();
 
-// 4. Store your basic details in localstorage of the browser every time the page loads, then console them and finally delete them after 1 minute of the page load. 
+// 4. Store your basic details in localstorage of the browser every time the page loads, then console them and finally delete them after 1 minute of the page load.
 
-function question4() {
-  localStorage.setItem("firstName", "Joshva");
-  localStorage.setItem("secondName", "D");
-  localStorage.setItem("Age", 22);
-  console.log(localStorage.getItem("firstName"));
-  console.log(localStorage.getItem("secondName"));
-  console.log(localStorage.getItem("Age"));
+localStorage.setItem("firstName", "Joshva");
+localStorage.setItem("secondName", "D");
+localStorage.setItem("Age", 22);
+console.log(localStorage.getItem("firstName"));
+console.log(localStorage.getItem("secondName"));
+console.log(localStorage.getItem("Age"));
 
-  setTimeout(function () {
-    localStorage.clear();
-    if (localStorage.length === 0) {
-      console.log("Local Storage is Empty");
-    }
-  }, 60 * 1000);
-}
-
-question4();
+setTimeout(function () {
+  localStorage.clear();
+  if (localStorage.length === 0) {
+    console.log("Local Storage is Empty");
+  }
+}, 60 * 1000);
 
 // 5. Redirect to the homepage of google from the console.
 
-setTimeout(function(){
-    location.href = 'https://www.google.co.in/';
+setTimeout(function () {
+  location.href = "https://www.google.co.in/";
 }, 65 * 1000);
-
